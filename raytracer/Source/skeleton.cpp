4d832aa7675e0e2ff3fd06c56d334ba14848c3a8
TheObnoxiousFrog <jl15216@my.bristol.ac.uk>
2018-02-26 14:52:58 +0000

    light power

--- raytracer/Source/skeleton.cpp
+++ raytracer/Source/skeleton.cpp
@@ -178,7 +178,7 @@ vec3 DirectLight(const Intersection& i, vector<Triangle> triangles, Light light)
     float dist = glm::length(light.position - i.position);
     vec4 n_hat = glm::normalize(triangles[i.triangleIndex].normal);
 
-    vec3 lightColour = light.colour * glm::max(glm::dot(r_hat, n_hat), 0.0f) /
+    vec3 lightColour = light.power * glm::max(glm::dot(r_hat, n_hat), 0.0f) /
         (float) (4.0f * glm::pi<float>() * glm::pow<float>(dist,2));
 
     Intersection closestIntersection = {
