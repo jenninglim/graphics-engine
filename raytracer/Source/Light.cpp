d8e9b58f64445ba428333e174407128e0f6b5882
TheObnoxiousFrog <jl15216@my.bristol.ac.uk>
2018-02-26 20:26:36 +0000

    ABS

---
+++ raytracer/Source/Light.cpp
@@ -0,0 +1,34 @@
+#include "Light.h"
+#include "glm/ext.hpp"
+
+using namespace std;
+using glm::vec3;
+using glm::vec4;
+
+vec3 DirectLight(const Intersection& i, vector<Triangle> triangles, Light light)
+{
+    vec4 r_hat = glm::normalize(light.position - i.position);
+    float dist = glm::length(light.position - i.position);
+    vec4 n_hat = glm::normalize(triangles[i.triangleIndex].normal);
+
+    vec3 lightColour = light.power * glm::max(glm::dot(r_hat, n_hat), 0.0f) /
+        (float) (4.0f * glm::pi<float>() * glm::pow<float>(dist,2));
+
+    Intersection closestIntersection = {
+                light.position,
+                std::numeric_limits<float>::max(),
+                0};
+
+    if (ClosestIntersection(i.position, r_hat, triangles, closestIntersection))
+    {
+         if (closestIntersection.distance < glm::length(light.position - i.position) &&
+                 closestIntersection.distance > EPSILON)
+            {
+                //cout << "distance" <<closestIntersection.distance<<endl;
+                //cout << "dist" <<glm::length(light.position - i.position)<<endl;
+                lightColour =  vec3(0,0,0);
+            }
+    }
+    lightColour += light.indirect_light;
+    return lightColour;
+}
