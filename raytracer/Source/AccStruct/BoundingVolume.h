66cda4618000b679ded5c11d9e96e6616563fc03
TheObnoxiousFrog <jl15216@my.bristol.ac.uk>
2018-03-29 13:44:59 +0100

    more abstraction

---
+++ raytracer/Source/AccStruct/BoundingVolume.h
@@ -0,0 +1,17 @@
+#ifndef BOUNDINGVOL_H
+#define BOUNDINGVOL_H
+
+#include <glm/glm.hpp>
+
+using glm::vec3;
+
+class BoundingVolume
+{
+    public:
+        vec3 min;
+        vec3 max;
+        BoundingVolume();
+        BoundingVolume(vec3 min, vec3 max);
+};
+
+#endif
---
+++ raytracer/Source/Shader/Collision.h
@@ -0,0 +1,12 @@
+#ifndef COLLISION_H
+#define COLLISION_H
+
+#include "BVH.h"
+#include "Ray.h"
+#include <glm/glm.hpp>
+
+bool collision(BVH bvh, Ray r, Intersection &closestI);
+
+bool collision(BVH bvh, Cone r, Intersection &closestI);
+
+#endif
